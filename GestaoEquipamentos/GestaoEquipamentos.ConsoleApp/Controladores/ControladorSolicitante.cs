ab592860a11841bce546bcf208cb9058c00e185a
Diconcili <80936673+Diconcili@users.noreply.github.com>
2021-05-07 16:30:59 -0300

    Add files via upload

---
+++ GestaoEquipamentos/GestaoEquipamentos.ConsoleApp/Controladores/ControladorSolicitante.cs
@@ -0,0 +1,57 @@
+﻿using GestaoEquipamentos.ConsoleApp.Dominio;
+using System;
+
+namespace GestaoEquipamentos.ConsoleApp.Controladores
+{
+    public class ControladorSolicitante : ControladorBase
+    {
+        public string RegistrarSolicitante(int id, string nome, string email, string nTelefone)
+        {
+            Solicitante solicitante = null;
+
+            int posicao;
+
+            if (id == 0)
+            {
+                solicitante = new Solicitante();
+                posicao = ObterPosicaoVaga();
+            }
+            else
+            {
+                posicao = ObterPosicaoOcupada(new Solicitante(id));
+                solicitante = (Solicitante)registros[posicao];
+            }
+
+            solicitante.nome = nome;
+            solicitante.nTelefone = nTelefone;
+            solicitante.email = email;
+
+
+            string resultadoValidacao = solicitante.Validar();
+
+            if (resultadoValidacao == "EQUIPAMENTO_VALIDO")
+                registros[posicao] = solicitante;
+
+            return resultadoValidacao;
+        }
+
+        public bool ExcluirSolicitante(int idSelecionado)
+        {
+            return ExcluirRegistro(new Equipamento(idSelecionado));
+        }
+
+        public Solicitante SelecionarSolicitantePorId(int id)
+        {
+            return (Solicitante)SelecionarRegistroPorId(new Solicitante(id));
+        }
+
+        public Solicitante[] SelecionarTodosSolicitantes()
+        {
+            Solicitante[] solicitantesAux = new Solicitante[QtdRegistrosCadastrados()];
+
+            Array.Copy(SelecionarTodosRegistros(), solicitantesAux, solicitantesAux.Length);
+
+            return solicitantesAux;
+        }
+    }
+}
